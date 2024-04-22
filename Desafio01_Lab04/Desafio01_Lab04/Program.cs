39e15d4fa4619056569e3ef60c604ef0a83ccbc8
Yessi Ramírez <jramirezs26>
2024-04-21 23:32:28 -0600

    first commit

---
+++ Desafio01_Lab04/Desafio01_Lab04/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+class Program { 
+    static void Main(string[] args)
+    {   Calculadora calculadora = new Calculadora("MarcaC", "SerieC");
+        Console.WriteLine($"1.Sumar: {calculadora.Sumar(1, 2)}");
+        Console.WriteLine($"2.Restar: {calculadora.Restar(2, 1)}");
+        Console.WriteLine($"3.Multiplicación: {calculadora.Multiplicar(2, 3)}");
+        Console.WriteLine($"4.División: {calculadora.Dividir(15, 3)}");
+
+        Calculadoracientifica calculadoraC = new Calculadoracientifica("MarcaCC", "SerieCC");
+        Console.WriteLine($"5.Potencia: {calculadoraC.Potencia(1, 2)}");
+        Console.WriteLine($"6.Raíz cuadrada: {calculadoraC.Raiz(2, 1)}");
+        Console.WriteLine($"7.Módulo: {calculadoraC.Modulo(100, 2)}");
+        Console.WriteLine($"8.Logaritmo: {calculadoraC.Logaritmo(100, 10)}");
+    }
+}
+public class Calculadora
+{ public string Marcac { get; set; }
+    public string Seriec { get; set; }
+    public Calculadora(string Marca, string Serie)
+    {Marcac = Marca;
+        Seriec = Serie; }
+    public double Sumar(double a, double b)
+    { return a + b; }
+    public double Restar(double a, double b)
+    {return a - b;}
+    public double Multiplicar(double a, double b)
+    { return a * b; }
+    public double Dividir(double a, double b)
+    { return a / b;}
+}
+public class Calculadoracientifica : Calculadora
+{public Calculadoracientifica(string Marcacc, string Seriecc) : base(Marcacc, Seriecc)
+    {}public double Potencia(double baseNum, double exponente)
+    {return Math.Pow(baseNum, exponente);}
+    public double Raiz(double numero, double raiz)
+    {return Math.Pow(numero, 1 / raiz); }
+    public double Modulo(double a, double b)
+    {return a % b;}
+    public double Logaritmo(double numero, double baseLogaritmo)
+    {return Math.Log(numero, baseLogaritmo);}
+}
+
---
+++ Desafio02_Lab04/Desafio02_Lab04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+interface INotificable
+{void Notifica();}
+class NotificacionEmail : INotificable
+{ private string DireccionCorreo;
+    public NotificacionEmail(string direccionCorreo) => this.DireccionCorreo = direccionCorreo;
+    public void Notifica() => Console.WriteLine($"Correo electrónico: {DireccionCorreo}");}
+class NotificacionWhatsap : INotificable
+{ private string NumeroTelefono;
+    public NotificacionWhatsap(string numeroTelefono) => this.NumeroTelefono = numeroTelefono;
+    public void Notifica() => Console.WriteLine($"Mensaje de Whatsapp: {NumeroTelefono}");}
+class NotificacionSMS : INotificable
+{private string numeroTelefono;
+    public NotificacionSMS(string numeroTelefono) => this.numeroTelefono = numeroTelefono;
+    public void Notifica() => Console.WriteLine($"Mensaje de SMS: {numeroTelefono}");}
+class Program
+{ static void Main(string[] args)
+    {var notificacionEmail = new NotificacionEmail("jramirezs26@miumg.edu.gt");
+        var notificacionWhatsap = new NotificacionWhatsap("47585847");
+        var notificacionSMS = new NotificacionSMS("47585847");
+
+        notificacionEmail.Notifica();
+        notificacionWhatsap.Notifica();
+        notificacionSMS.Notifica();}
+}
